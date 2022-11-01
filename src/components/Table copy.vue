d82f61fbb64ac9bd996b38cda84298264a077af2
namwhanmtk <mtknamwhan@gmail.com>
2022-11-01 23:06:03 +0700

    fixed header

---
+++ src/components/Table copy.vue
@@ -0,0 +1,141 @@
+<script>
+import { formatNumber } from "@utils/helper";
+
+export default {
+  name: "Card",
+  props: {
+    data: {
+      type: Array,
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    formatNumber,
+  },
+  // created() {
+  //   // props are exposed on `this`
+  //   console.log(this.title)
+  // }
+};
+</script>
+<template>
+  <div class="custom-table table-responsive">
+    <table class="table align-middle table-borderless m-0">
+      <thead class="table-color">
+        <tr class="">
+          <th>เดือน</th>
+          <th>PC สะสมของตนเอง</th>
+          <th>PC สะสมของหน่วย</th>
+          <th>จำนวนรายของหน่วย</th>
+          <th>PC สะสมของสายงาน</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(item, index) in data"
+          :key="index"
+          :class="{
+            'table-first': index === 0,
+            'table-color font_medium': index === data.length - 1,
+          }"
+        >
+          <td>{{ item.name }}</td>
+          <td>{{ formatNumber(item.pc, 2) }}</td>
+          <td>{{ formatNumber(item.pc_team, 2) }}</td>
+          <td>{{ item.count }}</td>
+          <td>{{ formatNumber(item.pc_branch, 2) }}</td>
+        </tr>
+        <!-- <tr>
+          <td>Project Apollo</td>
+          <td>01/01/2021</td>
+          <td>31/06/2021</td>
+          <td>31/06/2021</td>
+          <td>31/06/2021</td>
+        </tr>
+        <tr>
+          <td>Project Apollo</td>
+          <td>01/01/2021</td>
+          <td>31/06/2021</td>
+          <td>31/06/2021</td>
+          <td>31/06/2021</td>
+        </tr>
+        <tr>
+          <td>Project Apollo</td>
+          <td>01/01/2021</td>
+          <td>31/06/2021</td>
+          <td>31/06/2021</td>
+          <td>31/06/2021</td>
+        </tr>
+        <tr>
+          <td>Project Apollo</td>
+          <td>01/01/2021</td>
+          <td>31/06/2021</td>
+          <td>31/06/2021</td>
+          <td>31/06/2021</td>
+        </tr>
+        <tr>
+          <td>Project Apollo</td>
+          <td>01/01/2021</td>
+          <td>31/06/2021</td>
+          <td>31/06/2021</td>
+          <td>31/06/2021</td>
+        </tr> -->
+      </tbody>
+      <!-- <tfoot>
+        <tr>
+          <td>Footer</td>
+          <td>01/01/2021</td>
+          <td>31/06/2021</td>
+          <td>31/06/2021</td>
+          <td>31/06/2021</td>
+        </tr>
+      </tfoot> -->
+    </table>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.custom-table {
+  border: 1px solid #bababa;
+  td {
+    border-right: 1px solid #bababa;
+    border-color: #bababa !important;
+    text-align: right;
+    padding: 0 24px;
+  }
+  th {
+    border-right: 1px solid #bababa;
+    border-color: #bababa !important;
+  }
+  th,
+  td:first-child {
+    text-align: center;
+  }
+
+  th:last-child,
+  td:last-child {
+    border-right: none;
+  }
+  th,
+  td {
+    line-height: 36px;
+    font-size: 16px;
+    height: 40px;
+  }
+  th {
+    height: 40px;
+    padding: 0;
+    vertical-align: middle;
+  }
+  .table-color {
+    background-color: #daecf2;
+    td {
+    }
+  }
+  .table-first td {
+    padding-top: 20px;
+  }
+}
+</style>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -21,121 +21,119 @@ export default {
 };
 </script>
 <template>
-  <div class="custom-table table-responsive">
-    <table class="table align-middle table-borderless m-0">
-      <thead class="table-color">
-        <tr class="">
-          <th>เดือน</th>
-          <th>PC สะสมของตนเอง</th>
-          <th>PC สะสมของหน่วย</th>
-          <th>จำนวนรายของหน่วย</th>
-          <th>PC สะสมของสายงาน</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="(item, index) in data"
-          :key="index"
-          :class="{
-            'table-first': index === 0,
-            'table-color font_medium': index === data.length - 1,
-          }"
-        >
-          <td>{{ item.name }}</td>
-          <td>{{ formatNumber(item.pc, 2) }}</td>
-          <td>{{ formatNumber(item.pc_team, 2) }}</td>
-          <td>{{ item.count }}</td>
-          <td>{{ formatNumber(item.pc_branch, 2) }}</td>
-        </tr>
-        <!-- <tr>
-          <td>Project Apollo</td>
-          <td>01/01/2021</td>
-          <td>31/06/2021</td>
-          <td>31/06/2021</td>
-          <td>31/06/2021</td>
-        </tr>
-        <tr>
-          <td>Project Apollo</td>
-          <td>01/01/2021</td>
-          <td>31/06/2021</td>
-          <td>31/06/2021</td>
-          <td>31/06/2021</td>
-        </tr>
-        <tr>
-          <td>Project Apollo</td>
-          <td>01/01/2021</td>
-          <td>31/06/2021</td>
-          <td>31/06/2021</td>
-          <td>31/06/2021</td>
-        </tr>
-        <tr>
-          <td>Project Apollo</td>
-          <td>01/01/2021</td>
-          <td>31/06/2021</td>
-          <td>31/06/2021</td>
-          <td>31/06/2021</td>
-        </tr>
-        <tr>
-          <td>Project Apollo</td>
-          <td>01/01/2021</td>
-          <td>31/06/2021</td>
-          <td>31/06/2021</td>
-          <td>31/06/2021</td>
-        </tr> -->
-      </tbody>
-      <!-- <tfoot>
-        <tr>
-          <td>Footer</td>
-          <td>01/01/2021</td>
-          <td>31/06/2021</td>
-          <td>31/06/2021</td>
-          <td>31/06/2021</td>
-        </tr>
-      </tfoot> -->
-    </table>
+  <div class="box-table">
+    <div class="header table-color">
+      <div>เดือน</div>
+      <div>PC สะสมของตนเอง</div>
+      <div>PC สะสมของหน่วย</div>
+      <div>จำนวนรายของหน่วย</div>
+      <div>PC สะสมของสายงาน</div>
+    </div>
+    <div class="wrap-body">
+      <div
+        class="body"
+        v-for="(item, index) in data"
+        :key="index"
+        :class="{
+          last: index === data.length - 2,
+          footer: index === data.length - 1,
+        }"
+      >
+        <div>{{ item.name }}</div>
+        <div>{{ formatNumber(item.pc, 2) }}</div>
+        <div>{{ formatNumber(item.pc_team, 2) }}</div>
+        <div>{{ item.count }}</div>
+        <div>{{ formatNumber(item.pc_branch, 2) }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.custom-table {
+.box-table {
   border: 1px solid #bababa;
-  td {
+  font-family: "regular";
+  font-size: 16px;
+  position: relative;
+}
+.header {
+  position: absolute;
+  width: 100%;
+  display: flex;
+  font-family: "medium";
+  background-color: #b5dae6;
+  padding-right: 6px;
+  div {
+    display: flex;
+    align-items: center;
     border-right: 1px solid #bababa;
-    border-color: #bababa !important;
-    text-align: right;
-    padding: 0 24px;
+    width: max-content;
+    flex: 1;
+    justify-content: center;
+    height: 40px;
   }
-  th {
-    border-right: 1px solid #bababa;
-    border-color: #bababa !important;
+  div:last-child {
+    border: none;
   }
-  th,
-  td:first-child {
-    text-align: center;
-  }
+}
+.wrap-body {
+  margin-top: 40px;
+  max-height: 463px;
+  overflow: hidden;
+  overflow-y: scroll;
+  margin-bottom: 40px;
+}
 
-  th:last-child,
-  td:last-child {
-    border-right: none;
-  }
-  th,
-  td {
-    line-height: 36px;
+.body {
+  display: flex;
+  border-bottom: 1px solid #bababa;
+
+  div {
+    padding: 13px 24px;
+    width: 130px;
+    display: flex;
+    align-items: center;
+    border-right: 1px solid #bababa;
+    width: max-content;
+    flex: 1;
+    justify-content: flex-end;
     font-size: 16px;
-    height: 40px;
   }
-  th {
-    height: 40px;
-    padding: 0;
-    vertical-align: middle;
+  div:first-child {
+    justify-content: center;
   }
-  .table-color {
-    background-color: #daecf2;
-    td {
-    }
+  div:last-child {
+    border: none;
   }
-  .table-first td {
-    padding-top: 20px;
+}
+.body:first-child {
+  div {
+    padding-top: 25px;
   }
+}
+.body:last-child,
+.last {
+  border: none;
+}
+.last div {
+  padding-bottom: 25px;
+}
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background-color: #b5dae6;
+  font-family: "medium";
+  padding-right: 6px;
+}
+.wrap-body::-webkit-scrollbar {
+  -webkit-appearance: none;
+  width: 6px;
+}
+
+.wrap-body::-webkit-scrollbar-thumb {
+  border-radius: 6px;
+  border: 2px solid white; /* should match background, can't be transparent */
+  background-color: #cbcbcb;
 }
 </style>
